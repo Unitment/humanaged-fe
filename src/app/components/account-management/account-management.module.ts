55193c38c59d8f91527233d6138a20b51015fb57
huutinhlg2d <minkyshinyu@gmail.com>
2022-02-24 23:23:24 +0700

    update merge dev-HoangPT12 - 240222.1123

--- src/app/components/account-management/account-management.module.ts
+++ src/app/components/account-management/account-management.module.ts
@@ -1,6 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatIconModule } from '@angular/material/icon';
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatSelectModule } from '@angular/material/select';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { HttpClientModule } from '@angular/common/http';
@@ -21,9 +31,19 @@ import { UserInfoComponent } from './user-info/user-info.component';
     CommonModule,
     AccountManagementRoutingModule,
     BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatButtonModule,
+    MatInputModule,
     FlexLayoutModule,
     HttpClientModule,
     FormsModule,
+    MatButtonToggleModule ,
+    MatIconModule,
+    MatBadgeModule,
+    MatSelectModule,
+    MatMenuModule
   ],
 })
 export class AccountManagementModule { }

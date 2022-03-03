8faccbed4fd23a12f80fbc6e0e30203208abd0ca
huutinhlg2d <minkyshinyu@gmail.com>
2022-03-03 11:12:42 +0700

    commit merge dev-TinhHH 030322.1157

--- src/app/components/project-management/project-management.module.ts
+++ src/app/components/project-management/project-management.module.ts
@@ -1,4 +1,4 @@
-import {NgModule} from '@angular/core';
+import {NgModule, NO_ERRORS_SCHEMA} from '@angular/core';
 import {CommonModule} from '@angular/common';
 
 import {ProjectManagementRoutingModule} from '../../routing/project-management-routing.module';

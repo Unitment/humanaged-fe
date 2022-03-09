3c875c34de6e0d16cf7b207bc21258e536063dcd
Tan Hoang <90441361+tanhoang-fe@users.noreply.github.com>
2022-03-09 17:04:14 +0700

    empFilter+empDialog+paginator

--- src/app/components/employee-management/employee-table/employee-table.component.ts
+++ src/app/components/employee-management/employee-table/employee-table.component.ts
@@ -2,6 +2,8 @@ import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { employee } from '../model/employee';
 import { EmployeeService } from 'src/app/services/employee.service';
+import { MatTableDataSource } from '@angular/material/table';
+import { EmpFilter } from 'src/app/model/employee/EmpFilter';
 @Component({
   selector: 'app-employee-table',
   templateUrl: './employee-table.component.html',
@@ -27,22 +29,36 @@ export class EmployeeTableComponent implements OnInit {
 
   ngOnInit(): void {
     this.employeeTable();
+    this.dataSource.filterPredicate = function (record,filter) {
+      return record.account?.accountName.toLocaleLowerCase() == filter.toLocaleLowerCase();
   }
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+
 
 
   employeeTable() {
     this._service.employeeTable().subscribe(
       data=>{
-        this.dataSource.push(...data);
-        console.log(this.dataSource);
+        this.empData.push(...data);
         this.isLoaded=true;
-        this.cdr.markForCheck()
+        this.dataSource.paginator = this.paginator;
       }
     )
   }
+  dataSource = new MatTableDataSource(this.empData);
+  dataSourceFilters = new MatTableDataSource(this.empData);
 
   onEditClick(id: string) {
     this.router.navigate(['/employee/edit', id]);
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
   }
 
 
